b8ea1d80992a7e4dab60d68623924627ed98aeea
khushal sarode <khushalsarode321@gmail.com>
2023-04-24 02:11:22 +0530

    xoxo

--- Demo.java
+++ Demo.java
@@ -1,5 +1,10 @@
-class Demo{
-        public static void main(String args []){
-            System.out.println("HelloWorld from F1");
-        }
+class Demo {
+    public static void main(String args[]) {
+        System.out.println("HelloWorld from F1");
+    }
+
+class Demo {
+    public static void main(String args[]) {
+        System.out.println("HelloWorld f2");
+    }
 }
